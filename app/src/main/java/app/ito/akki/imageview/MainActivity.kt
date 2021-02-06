2c3b27f306315fc3ac49581be26ac890d0906a86
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-02-06 20:40:53 +0900

    Initial commit

---
+++ app/src/main/java/app/ito/akki/imageview/MainActivity.kt
@@ -0,0 +1,18 @@
+package app.ito.akki.imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import coil.api.load
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val url = "https://life-is-tech.com/materials/images/logo-square.png"
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        imageView.load(url)
+    }
+}
